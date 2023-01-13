f988bc31819d544a1c961bac289954fdf38ff88a
Sobiwa <kevin.b.collister@gmail.com>
2023-01-13 18:00:39 -0500

    nothing

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
+              ['@babel/preset-env', { targets: 'defaults' }]
             ]
           }
         }
